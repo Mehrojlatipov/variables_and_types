7a047291271222702e5eb4353165f47f09132f16
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-17 19:58:28 +0500

    begin08

--- begin08.py
+++ begin08.py
@@ -1,7 +1,9 @@
 # Assign 5 (int value) to 'a' variable;
-
+a = 5
 # Assign 4 (int value) to 'b' variable;
-
+b = 4
 # Assign 8 (int value) to 'c' variable;
-
+c = 8
 # Print the result of that expression 'https://github.com/xakimovallamurod/variables_and_types/blob/main/README.md#begin08';
+k = a * b / c
+print (k)
